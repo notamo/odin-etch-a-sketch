0d2f4d1db534a437ac33f23e72bbe232a7378b2a
notamo <203415281+notamo@users.noreply.github.com>
2025-11-02 20:27:55 +0100

    Setup hover effect

--- script.js
+++ script.js
@@ -9,13 +9,26 @@ let drawGrid = () => {
         
         while (tilesPerRow--) {
             const div = document.createElement("div");
-            div.classList.add("tile");
+            // div.classList.add("tile");
+            div.id = `tile-r${rowsCount}-c${tilesPerRow}`;
+            
             rowDiv.appendChild(div);
         }
         tilesPerRow = 16;
 
         container.appendChild(rowDiv);
     }
+
+    container.addEventListener("mouseover", (e) => {
+        targetElement = e.target;
+
+        if (targetElement.id) {
+            // console.log(`Mouse entered tile ${targetElement.id}`);
+            targetElement.style.backgroundColor = "red";
+        }
+
+        
+    });
 }
 
 drawGrid();

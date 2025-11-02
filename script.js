65a22005615face4c36b4ddedf17e5882c4a4af8
notamo <203415281+notamo@users.noreply.github.com>
2025-11-02 19:42:24 +0100

    Draw 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello from script.js");
+let drawGrid = () => {
+    const container = document.querySelector(".container");
+
+    let rowsCount = 16;
+    let tilesPerRow = 16;
+    while (rowsCount--) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row")
+        
+        while (tilesPerRow--) {
+            const div = document.createElement("div");
+            div.classList.add("tile");
+            rowDiv.appendChild(div);
+        }
+        tilesPerRow = 16;
+
+        container.appendChild(rowDiv);
+    }
+}
+
+drawGrid();
